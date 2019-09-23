e7349be1bbba67dd281ef5c7e421e237f7db9fbe
Dmitry Grebenschikov <dvengroff@gmail.com>
2019-09-23 18:49:25 +0400

    add tests

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -13,9 +13,26 @@ class AppTest extends TestCase
      *
      * @return void
      */
-    public function testHomePage()
+    public function testIndexPage()
     {
         $this->get('/');
         $this->assertResponseStatus(200);
+    }
+
+    public function testDomainPage()
+    {
+        $domain = factory(\App\Domain::class)->create();
+        $this->get(route('domains.show', ['id' => $domain->id]));
+
+        $this->assertResponseStatus(200);
+    }
+
+    public function testDomainStore()
+    {
+        $domain = factory(\App\Domain::class)->make();
+        $this->post(route('domains.store'), ['url' => $domain->name]);
+
+        $this->seeInDatabase('domains', ['name' => $domain->name]);
+        $this->assertResponseStatus(302);
     }
 }
